7401ef82aca5cb01302bb321de54631c5012aad5
lijianqiang <bytehonor@126.com>
2019-12-16 21:03:30 +0800

    0.0.4 style

---
+++ src/main/java/com/bytehonor/sdk/builder/html/style/MdStyle.java
@@ -0,0 +1,46 @@
+package com.bytehonor.sdk.builder.html.style;
+
+/**
+ * 
+ * https://github.com/doocs/md
+ * 
+ * @author lijianqiang
+ *
+ */
+public class MdStyle {
+
+    public static final String FONT = "font-family:-apple-system-font,BlinkMacSystemFont, Helvetica Neue, PingFang SC, Hiragino Sans GB , Microsoft YaHei UI , Microsoft YaHei ,Arial,sans-serif;";
+
+    public static final String H1 = "text-align:center;color:#3f3f3f;line-height:1.75;font-size:1.2em;font-weight:bold;display:table;margin:2em auto 1em;padding:0 1em;border-bottom:2px solid rgba(15, 76, 129, 0.9);margin-top: 0"
+            + FONT;
+
+    public static final String H2 = "text-align:center;color:#fff;line-height:1.75;font-size:1.1em;font-weight:bold;display:table;margin:4em auto 2em;padding:0 0.2em;background:rgba(15, 76, 129, 0.9)"
+            + FONT;
+
+    public static final String H3 = "text-align:left;color:#3f3f3f;line-height:1.2;font-size:1.1em;font-weight:bold;margin:2em 8px 0.75em 0;padding-left:8px;border-left:3px solid rgba(15, 76, 129, 0.9)"
+            + FONT;
+
+    public static final String H4 = "text-align:left;color:rgba(15, 76, 129, 0.9);line-height:1.75;font-size:1em;font-weight:bold;margin:2em 8px 0.5em"
+            + FONT;
+
+    public static final String P = "text-align:left;color:#3f3f3f;line-height:1.75;font-size:14px;margin:1.5em 8px;letter-spacing:0.1em"
+            + FONT;
+
+    public static final String FIGURE = "text-align:left;color:#3f3f3f;line-height:1.75;font-size:14px;margin:1.5em 8px"
+            + FONT;
+
+    public static final String CODE = "text-align:left;color:#d14;line-height:1.75;font-family:Menlo, Operator Mono, Consolas, Monaco, monospace;font-size:90%;background:rgba(27,31,35,.05);padding:3px 5px;border-radius:4px;white-space:normal";
+
+    public static final String STRONG = "text-align:left;color:rgba(15, 76, 129, 0.9);line-height:1.75;font-size:14px;font-weight:bold"
+            + FONT;
+
+    public static final String IMG = "text-align:left;color:#3f3f3f;line-height:1.75;font-size:14px;border-radius:4px;display:block;margin:0.1em auto 0.5em;width:100% !important";
+
+    public static final String BLOCKQUOTE = "text-align:left;color:#FEEEED;line-height:1.75;font-size:14px;font-style:normal;border-left:none;padding:1em;border-radius:4px;background:rgba(27,31,35,.05);margin:2em 8px"
+            + FONT;
+
+    public static final String BLOCKQUOTE_P = "text-align:left;color:rgb(80, 80, 80);line-height:1.75;font-family:PingFangSC-light, PingFangTC-light, Open Sans, Helvetica Neue, sans-serif;font-size:1em;letter-spacing:0.1em;display:inline";
+
+    public static final String P_BR_AFTER_P = "text-align:left;color:#3f3f3f;line-height:1.75;font-size:14px;margin:1.5em 8px;letter-spacing:0.1em";
+
+}
