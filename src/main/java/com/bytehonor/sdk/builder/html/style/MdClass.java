dea1fc23ae06c131514720bbc9a0560ef8987c0e
lijianqiang <bytehonor@126.com>
2020-03-12 21:30:01 +0800

    0.1.5

---
+++ src/main/java/com/bytehonor/sdk/builder/html/style/MdClass.java
@@ -0,0 +1,35 @@
+package com.bytehonor.sdk.builder.html.style;
+
+/**
+ * https://m.bytehonor.com/res/css/news-md-style.css
+ * 
+ * @author lijianqiang
+ *
+ */
+public class MdClass {
+
+    public static final String CONTAINER = "news-md-container";
+
+    public static final String H1 = "news-md-h1";
+
+    public static final String H2_1 = "news-md-h2-1";
+
+    public static final String H2_2 = "news-md-h2-2";
+
+    public static final String H3 = "news-md-h3";
+
+    public static final String H4 = "news-md-h4";
+
+    public static final String P = "news-md-p";
+
+    public static final String FIGURE = "news-md-figure";
+
+    public static final String CODE = "news-md-code";
+
+    public static final String STRONG = "news-md-strong";
+
+    public static final String IMG = "news-md-img";
+
+    public static final String BLOCKQUOTE = "news-md-blockquote";
+
+}
